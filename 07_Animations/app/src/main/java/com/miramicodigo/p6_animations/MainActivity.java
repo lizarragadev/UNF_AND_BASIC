6f1ab45ae68d34841c17f5209b089596d7976922
Gustavo Lizarraga <lizarraga.gux@gmail.com>
2017-10-19 16:46:23 -0400

    mensaje

--- 07_Animations/app/src/main/java/com/miramicodigo/p6_animations/MainActivity.java
+++ 07_Animations/app/src/main/java/com/miramicodigo/p6_animations/MainActivity.java
@@ -30,58 +30,74 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void zoomin(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.zoom_in);
+        ivOctocat.startAnimation(animation);
     }
 
     public void zoomout(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.zoom_out);
+        ivOctocat.startAnimation(animation);
     }
 
     public void fadein(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        ivOctocat.startAnimation(animation);
     }
 
     public void fadeout(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        ivOctocat.startAnimation(animation);
     }
 
     public void rotate(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+        ivOctocat.startAnimation(animation);
     }
 
     public void move(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.move);
+        ivOctocat.startAnimation(animation);
     }
 
     public void slideup(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.slide_up);
+        ivOctocat.startAnimation(animation);
     }
 
     public void slidedown(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.slide_down);
+        ivOctocat.startAnimation(animation);
     }
 
     public void blink(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.blink);
+        ivOctocat.startAnimation(animation);
     }
 
     public void bounce(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.bounce);
+        ivOctocat.startAnimation(animation);
     }
 
     public void crossfade(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.fade_out);
+        Animation animation2 = AnimationUtils.loadAnimation(this, R.anim.fade_in);
+        ivOctocat.startAnimation(animation);
+        ivOctocat2.startAnimation(animation2);
     }
 
     public void sequential(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.sequential);
+        ivOctocat.startAnimation(animation);
     }
 
     public void together(View v) {
-
+        animation = AnimationUtils.loadAnimation(this, R.anim.together);
+        ivOctocat.startAnimation(animation);
     }
 
     public void stop(View v) {
-
+        ivOctocat.clearAnimation();
+        ivOctocat2.clearAnimation();
     }
 }
